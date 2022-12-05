34b37c03891ad8d12aae8731e82384e3f26bfe96
Utkp5 <utkp200@gmail.com>
2022-12-05 21:34:33 +0530

    bookshala frontend

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import { HiMenu } from "react-icons/hi";
 import { HiX } from "react-icons/hi";
 import { useState } from 'react';
+import logo from "../../images/logo.png"
 import './Navbar.css';
 
 
@@ -12,12 +13,14 @@ function App() {
 
        <div className='navbar'>
 
-            <div className='logo'>LOGO</div>
+            <div>
+            <a href="/"><img src={logo} alt="" className='logo'/></a>
+            </div>
 
 
               <ul className={menu ? 'navbar_mobile_css' :  'navbar_ul'}>
                 <li><a href="">Home</a></li>
-                <li><a href="">About Us</a></li>
+                <li><a href="">About</a></li>
                 <li><a href="">Blog</a></li>
                 <li><a href="">Shop</a></li>
                 <li><a href="">Contact</a></li>

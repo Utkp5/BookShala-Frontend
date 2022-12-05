df9f6f3064df457c648099038afb7171c58de5a6
Utkp5 <utkp200@gmail.com>
2022-12-05 19:50:00 +0530

    bookshala frontend

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import { HiMenu } from "react-icons/hi";
+import { HiX } from "react-icons/hi";
+import { useState } from 'react';
+import './Navbar.css';
+
+
+function App() {
+  const [menu,setmenu] = useState(false);
+
+  return (
+    <div className="App">
+
+       <div className='navbar'>
+
+            <div className='logo'>LOGO</div>
+
+
+              <ul className={menu ? 'navbar_mobile_css' :  'navbar_ul'}>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Movies</a></li>
+                <li><button className='button_signup'>Signup</button></li>
+                <li><button className='button_login'>Login</button></li>
+              </ul>
+              <div className='menu_icon' onClick={() => setmenu(!menu)}>
+                    {menu ? <HiX size={46} /> :<HiMenu size={46} />}
+              </div>
+
+       </div>
+
+    </div>
+  );
+}
+
+
+export default App;

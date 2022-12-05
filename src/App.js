4515abc916e6e94dcae9b6753c77aa373402d650
Utkp5 <utkp200@gmail.com>
2022-12-06 00:56:45 +0530

    bookshala frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-import Navbar from "./Components/Navbar/Navbar"
+import React from "react";
+import { Browserrouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-    <Navbar />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/About" element={<About />} />
+        <Route exact path="/Blog" element={<Blog />} />
+        <Route exact path="/Shop" element={<Shop />} />
+        <Route exact path="/Contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 

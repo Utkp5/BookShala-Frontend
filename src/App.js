1dde03888cfe9bea1246303aa7b16d23b33dc814
Utkp5 <utkp200@gmail.com>
2022-12-15 20:34:56 +0530

    bookshala frontend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React from "react";
+import React,{Suspense,lazy}from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Components/Home/Home";
-import About from "./Components/About/About"
-import Blog from "./Components/Blog/Blog"
-import Shop from "./Components/Shop/Shop"
-import Contact from "./Components/Contact/Contact"
-import Cart from "./Components/Cart/Cart";
-import Signup from "./Components/Signup/Signup";
-import Signin from "./Components/Signin/Signin";
+
+
+//instead of writing this we are using this because of loading import Home from "./Components/Home/Home"
+
+const  Home    = lazy(() => import("./Components/Home/Home"))
+const  About   = lazy(() => import("./Components/About/About"))
+const  Blog    = lazy(() => import("./Components/Blog/Blog"))
+const  Shop    = lazy(() => import("./Components/Shop/Shop"))
+const  Contact = lazy(() => import("./Components/Contact/Contact"))
+const  Cart    = lazy(() => import("./Components/Cart/Cart"))
+const  Signup  = lazy(() => import("./Components/Signup/Signup"))
+const  Signin  = lazy(() => import("./Components/Signin/Signin"))
+
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/About" element={<About />} />
-        <Route exact path="/Blog" element={<Blog />} />
-        <Route exact path="/Shop" element={<Shop />} />
-        <Route exact path="/Contact" element={<Contact />} />
-        <Route exact path="/Cart" element={<Cart />} />
-        <Route exact path="/Signup" element={<Signup />} />
-        <Route exact path="/Signin" element={<Signin />} />
-      </Routes>
-    </Router>
+    <Suspense fallback={<div>Loading....</div>}>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/About" element={<About />} />
+            <Route exact path="/Blog" element={<Blog />} />
+            <Route exact path="/Shop" element={<Shop />} />
+            <Route exact path="/Contact" element={<Contact />} />
+            <Route exact path="/Cart" element={<Cart />} />
+            <Route exact path="/Signup" element={<Signup />} />
+            <Route exact path="/Signin" element={<Signin />} />
+          </Routes>
+        </Router>
+    </Suspense>
   );
 }
 

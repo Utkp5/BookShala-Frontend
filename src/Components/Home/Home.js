0768d02c804ae7e3889bccbf7b8a67025f3a478e
Utkp5 <utkp200@gmail.com>
2022-12-21 00:01:02 +0530

    bookshala frontend

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import Navbar from "../Navbar/Navbar"
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import "./Home.css";
+import stand from "../../Assets/Banners/stand.png";
 
 function Home() {
   return (
     <div>
-    <Navbar />
+      <Navbar />
+      <div className="Home_banner_div">
+        <div>
+          <div>
+          <p>lorem10</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <img src={stand} alt="" className="Home_stand" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;

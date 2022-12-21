ca587c064c20110f662693140ad5964b31b517e5
Utkp5 <utkp200@gmail.com>
2022-12-21 20:59:18 +0530

    bookshala frontend

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from "../Navbar/Navbar"
+import "./About.css"
 
 function About() {
   return (
     <div>
       <Navbar />
+      <div className='about_bg'>
+        <div className='about_bg_div'>About</div>
+        <img src="https://bookpresstheme.com/html/assets/images/banner-image.png" alt="" />
+      </div>
     </div>
   )
 }

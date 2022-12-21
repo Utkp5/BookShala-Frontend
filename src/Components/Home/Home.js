faecf96413aa3d90f536753cf23fadabdfd1536a
Utkp5 <utkp200@gmail.com>
2022-12-21 19:57:33 +0530

    bookshala frontend

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,10 +4,17 @@ import "./Home.css";
 import stand from "../../Assets/Banners/stand.png";
 import Slider_home from "../../Views/Slider_home/Slider_home";
 
+//icons
+import { FaTruck } from "react-icons/fa";
+import { FaUserLock } from "react-icons/fa";
+import { FaRedoAlt } from "react-icons/fa";
+import { FaHeadset } from "react-icons/fa";
+
 function Home() {
   return (
     <div>
       <Navbar />
+
       <div className="Home_banner_div">
         <div className="Home_flex">
           <div className="Home_flex_div">
@@ -19,6 +26,39 @@ function Home() {
         </div>
         <Slider_home />
       </div>
+
+      <div className="diagram">
+            <div className="dia_div">
+                <FaTruck size={36} color="dodgerblue"/>
+                <div>
+                  <p className="dia_div1_p">Free Shipping</p>
+                  <p className="dia_div1_p1">Order over $50</p>
+                </div>
+            </div>
+            <div className="dia_div">
+              <FaUserLock size={36} color="dodgerblue"/>
+              <div>
+                <p className="dia_div1_p">Secure Payment</p>
+                <p className="dia_div1_p1">100% Secure Payment</p>
+              </div>
+            </div>
+            <div className="dia_div">
+              <FaRedoAlt size={36} color="dodgerblue"/>
+              <div>
+                 <p className="dia_div1_p">Easy Return</p>
+                 <p className="dia_div1_p1">10 days return policy</p>
+              </div>
+            </div>
+            <div className="dia_div">
+              <FaHeadset size={36} color="dodgerblue"/>
+              <div>
+              <p className="dia_div1_p">24/7 Support</p>
+              <p className="dia_div1_p1">Call us anytime</p>
+              </div>
+            </div>
+      </div>
+
+
     </div>
   );
 }

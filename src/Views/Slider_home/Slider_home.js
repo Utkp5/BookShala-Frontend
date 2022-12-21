c7fba7b96672436b48c705a486e2a842bec41233
Utkp5 <utkp200@gmail.com>
2022-12-21 17:10:32 +0530

    bookshala frontend

--- src/Views/Slider_home/Slider_home.js
+++ src/Views/Slider_home/Slider_home.js
@@ -1,6 +1,14 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import book_1 from "../../Assets/Homeimg/book-1.png"
+import book_2 from "../../Assets/Homeimg/book-2.png"
+import book_3 from "../../Assets/Homeimg/book-3.png"
+import book_4 from "../../Assets/Homeimg/book-4.png"
+import book_5 from "../../Assets/Homeimg/book-5.png"
+import book_6 from "../../Assets/Homeimg/book-6.png"
+import book_7 from "../../Assets/Homeimg/book-7.png"
+import book_8 from "../../Assets/Homeimg/book-8.png"
+import book_9 from "../../Assets/Homeimg/book-9.png"
 
 // Import Swiper styles
 import "swiper/css";
@@ -24,19 +32,23 @@ export default function App() {
         pagination={{
           clickable: true,
         }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
         navigation={true}
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide><img src={book_1} alt="" className="slider_homeimg"/></SwiperSlide>
-        <SwiperSlide><img src={book_1} alt="" className="slider_homeimg"/></SwiperSlide>
-        <SwiperSlide><img src={book_1} alt="" className="slider_homeimg"/></SwiperSlide>
-        <SwiperSlide><img src={book_1} alt="" className="slider_homeimg"/></SwiperSlide>
-        <SwiperSlide><img src={book_1} alt="" className="slider_homeimg"/></SwiperSlide>
-        <SwiperSlide><img src={book_1} alt="" className="slider_homeimg"/></SwiperSlide>
-        <SwiperSlide><img src={book_1} alt="" className="slider_homeimg"/></SwiperSlide>
-        <SwiperSlide><img src={book_1} alt="" className="slider_homeimg"/></SwiperSlide>
-        <SwiperSlide><img src={book_1} alt="" className="slider_homeimg"/></SwiperSlide>
+        <SwiperSlide><img src={book_2} alt="" className="slider_homeimg"/></SwiperSlide>
+        <SwiperSlide><img src={book_3} alt="" className="slider_homeimg"/></SwiperSlide>
+        <SwiperSlide><img src={book_4} alt="" className="slider_homeimg"/></SwiperSlide>
+        <SwiperSlide><img src={book_5} alt="" className="slider_homeimg"/></SwiperSlide>
+        <SwiperSlide><img src={book_6} alt="" className="slider_homeimg"/></SwiperSlide>
+        <SwiperSlide><img src={book_7} alt="" className="slider_homeimg"/></SwiperSlide>
+        <SwiperSlide><img src={book_8} alt="" className="slider_homeimg"/></SwiperSlide>
+        <SwiperSlide><img src={book_9} alt="" className="slider_homeimg"/></SwiperSlide>
       </Swiper>
     </div>
   );

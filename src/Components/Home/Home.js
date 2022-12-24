4d32f145fc3fd464500d342d1e3ad57592c21bcf
Utkp5 <utkp200@gmail.com>
2022-12-24 13:50:09 +0530

    bookshala frontend

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -79,6 +79,19 @@ function Home() {
 
       <Slider_home1 />
 
+      <div>
+        <h3 className="home_filter_h3">Popular Collections</h3>
+          <p className="home_filter_p">The Way You Choose</p>
+          <div className="home_filter">
+              <button className="home_filter_btn">Mystery</button>
+              <button className="home_filter_btn">Romance</button>
+              <button className="home_filter_btn">Thriller</button>
+              <button className="home_filter_btn">Horror</button>
+              <button className="home_filter_btn">Spritual</button>
+          </div>
+      </div>
+
+
 
     </div>
   );

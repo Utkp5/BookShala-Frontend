9e012bb2dbd57f68820a76a3af1669b7c4c18eba
Utkp5 <utkp200@gmail.com>
2022-12-26 19:26:24 +0530

    bookshala frontend

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,10 +1,79 @@
 import React from 'react'
 import Navbar from "../Navbar/Navbar"
+import Footer from "../Footer/Footer"
+import "./Blog.css"
 
 function Blog() {
   return (
     <div>
        <Navbar />
+
+       <div className='blog'>
+
+            <div className='blog_div1'>
+              <img className='blog_img' src="https://cdn.shopify.com/s/files/1/1288/2297/articles/blog_7_600x400_crop_center.jpg?v=1646452858" alt="" />
+                 <p className='blog_p1'>Significant reading has info</p>
+                 <p className='blog_p2'>04 March, 2022 \ HasTech Shopify Team</p>
+                 <p>The summer holidays are wonderful. Dressing for them can be significantly
+                  less so: Packing light is always at a premium, but one never wants to feel high, dry, and seriously 
+                  unstylish with all of the possible opportunities to pose for that most modern
+                  of vacation souvenirs, the fashionable Instagram.</p>
+                  <p>The fashion set stayed true to their roots this week, whether that be swirling 
+                  around the interior of a river boat in floor-length Giambattista Valli, à la Bianca 
+                  Brandolini D'Adda, locally leaning against the scenery in innately comfortable summer classics in Positano, as Lisa Love did,
+                  or heading for the beach in Louis Vuitton resort denim hot pants and monogrammed Speedy in tow,
+                  as Marie-Amélie Sauvé did (oh, the access!).</p>
+                  <p>Natasha Poly took in the ocean breeze in Formentera in a voluminous caftan, while Caroline Trentini's
+                  white tee, Vuitton skirt, and Stan Smiths in Sao Paulo were a decidedly more city-friendly take on early 
+                  August style (and will certainly transition more easily into fall). And while we treasure these lazy, 
+                  late summer days, fall—and its sartorial demands—is just around the corner: For inspiration, look no further than little North West's
+                  latest wardrobe addition, a clutch of pint-sized Balmain blazers that will have the tot segueing into the season sans problème.</p>
+            </div>
+
+            <div className='blog_div1'>
+              <img className='blog_img' src="https://cdn.shopify.com/s/files/1/1288/2297/articles/blog_6_600x400_crop_center.jpg?v=1646452719" alt="" />
+                 <p className='blog_p1'>Activities Book International</p>
+                 <p className='blog_p2'>04 March, 2022 \ HasTech Shopify Team</p>
+                 <p>The summer holidays are wonderful. Dressing for them can be significantly
+                  less so: Packing light is always at a premium, but one never wants to feel high, dry, and seriously 
+                  unstylish with all of the possible opportunities to pose for that most modern
+                  of vacation souvenirs, the fashionable Instagram.</p>
+                  <p>The fashion set stayed true to their roots this week, whether that be swirling 
+                  around the interior of a river boat in floor-length Giambattista Valli, à la Bianca 
+                  Brandolini D'Adda, locally leaning against the scenery in innately comfortable summer classics in Positano, as Lisa Love did,
+                  or heading for the beach in Louis Vuitton resort denim hot pants and monogrammed Speedy in tow,
+                  as Marie-Amélie Sauvé did (oh, the access!).</p>
+                  <p>Natasha Poly took in the ocean breeze in Formentera in a voluminous caftan, while Caroline Trentini's
+                  white tee, Vuitton skirt, and Stan Smiths in Sao Paulo were a decidedly more city-friendly take on early 
+                  August style (and will certainly transition more easily into fall). And while we treasure these lazy, 
+                  late summer days, fall—and its sartorial demands—is just around the corner: For inspiration, look no further than little North West's
+                  latest wardrobe addition, a clutch of pint-sized Balmain blazers that will have the tot segueing into the season sans problème.</p>
+            </div>
+
+            <div className='blog_div1'>
+              <img className='blog_img' src="https://cdn.shopify.com/s/files/1/1288/2297/articles/blog_5_600x400_crop_center.jpg?v=1646452601" alt="" />
+                 <p className='blog_p1'>International activities of the book</p>
+                 <p className='blog_p2'>04 March, 2022 \ HasTech Shopify Team</p>
+                 <p>The summer holidays are wonderful. Dressing for them can be significantly
+                  less so: Packing light is always at a premium, but one never wants to feel high, dry, and seriously 
+                  unstylish with all of the possible opportunities to pose for that most modern
+                  of vacation souvenirs, the fashionable Instagram.</p>
+                  <p>The fashion set stayed true to their roots this week, whether that be swirling 
+                  around the interior of a river boat in floor-length Giambattista Valli, à la Bianca 
+                  Brandolini D'Adda, locally leaning against the scenery in innately comfortable summer classics in Positano, as Lisa Love did,
+                  or heading for the beach in Louis Vuitton resort denim hot pants and monogrammed Speedy in tow,
+                  as Marie-Amélie Sauvé did (oh, the access!).</p>
+                  <p>Natasha Poly took in the ocean breeze in Formentera in a voluminous caftan, while Caroline Trentini's
+                  white tee, Vuitton skirt, and Stan Smiths in Sao Paulo were a decidedly more city-friendly take on early 
+                  August style (and will certainly transition more easily into fall). And while we treasure these lazy, 
+                  late summer days, fall—and its sartorial demands—is just around the corner: For inspiration, look no further than little North West's
+                  latest wardrobe addition, a clutch of pint-sized Balmain blazers that will have the tot segueing into the season sans problème.</p>
+            </div>
+       
+       </div>
+
+       <Footer />
+
     </div>
   )
 }

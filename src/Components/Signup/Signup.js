964ace30c2985f961f5c778d7536d3a069c9d27f
Utkp5 <utkp200@gmail.com>
2022-12-29 00:25:05 +0530

    bookshala frontend

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signup.css";
 import Navbar from "../Navbar/Navbar";
 import gif from "../../Assets/Images/signupGif.gif"
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 function Signup() {
+
+  const [firstName,setfirstName] = useState();
+  const [lastName,setlastName] = useState();
+  const [userEmail,setuserEmail] = useState();
+  const [password,setpassword] = useState();
+  const [confirmPassword,setconfirmpassword] = useState();
+
   return (
     <div>
       <Navbar />
@@ -15,14 +23,24 @@ function Signup() {
       <div class="login">
         <h3 id="sgn_h3">Create Account</h3>
         <div className="signupform">
-          <input className="input_signup" type="text" placeholder="First Name"/>
-          <input className="input_signup" id="input_signup" type="text" placeholder="Last Name" />
+          <input className="input_signup" type="text" name="firstName" placeholder="First Name" onChange={(e) => {
+            setfirstName(e.target.value)
+          }} />
+          <input className="input_signup" id="input_signup" type="text"name="lastName"  placeholder="Last Name" onChange={(e) => {
+            setlastName(e.target.value)
+          }} />
           <br />
-          <input className="input_signup" type="email" placeholder="E-mail Address"/>
+          <input className="input_signup" type="email" name="userEmail" placeholder="E-mail Address" onChange={(e) => {
+            setuserEmail(e.target.value)
+          }} />
           <br />
-          <input className="input_signup" type="password" placeholder="Password"/>
+          <input className="input_signup" type="password" name="password" placeholder="Password" onChange={(e) => {
+            setpassword(e.target.value)
+          }} />
           <br />
-          <input className="input_signup" type="password" placeholder="Confirm Password"/>
+          <input className="input_signup" type="password" name="confirmpassword" placeholder="Confirm Password" onChange={(e) => {
+            setconfirmpassword(e.target.value)
+          }} />
           <div class="remember_form">
             <span><input type="checkbox" /> &nbsp;Remember me</span>
             <span>You are creating an account, and agree to Bookshala <NavLink className="sgn_navlink">Terms of Service</NavLink> and <NavLink className="sgn_navlink">Privacy Policy</NavLink></span>

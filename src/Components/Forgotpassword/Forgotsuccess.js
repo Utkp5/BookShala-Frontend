0885d4c1996ccc252ff369ed37ea22347590f8e5
Utkp5 <utkp200@gmail.com>
2022-12-29 19:42:34 +0530

    bookshala frontend

---
+++ src/Components/Forgotpassword/Forgotsuccess.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Forgotsuccess.css"
+
+function Forgotsuccess() {
+  return (
+    <div>
+        <div className="subcard">
+        <i className="checkmark">✓</i>
+        </div>
+        <h1 className='subcard_h1'>Success</h1><br/><br/>
+        <p className='subcard_p'>Password Reset Successfully</p><p className="subcard_p1">Close This Window To Enjoy Our Website</p>
+    </div>
+  )
+}
+
+export default Forgotsuccess

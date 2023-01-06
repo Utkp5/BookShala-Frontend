1b51fc63138a00a1f722ad378344a63f21d3b564
Utkp5 <utkp200@gmail.com>
2023-01-06 21:03:11 +0530

    bookshala frontend

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from "../Navbar/Navbar"
 import { useCart } from 'react-use-cart'
+import "./Cart.css"
 
 function Cart() {
 
@@ -15,13 +16,39 @@ function Cart() {
     emptyCart,
   } = useCart();
 
+   if (isEmpty) 
+   {
+    <p>Your cart is empty</p> 
+   }   
+
   return (
     <div>
       <Navbar />
 
-      <div>
-       {isEmpty ? 'Your cart is empty' : `Your products`}
-      </div>
+      <table>
+          <tr>
+            <th>Book img</th>
+            <th>Book title</th>
+            <th>Book price</th>
+            <th>Total quantity</th>
+            <th>Book quantity</th>
+            <th>Book remove</th>
+          </tr>
+          {
+            items.map((item,id) => {
+              return (
+                <tr>
+                  <td><img src={item.bookImg} alt="" /></td>
+                  <td>{item.bookTitle}</td>
+                  <td>{item.price}</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              )
+            })
+          }
+      </table>
 
     </div>
   )

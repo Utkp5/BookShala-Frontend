f831ede09283c65b11efbd97a64abd2e1fd5114b
Utkp5 <utkp200@gmail.com>
2023-01-06 17:55:36 +0530

    bookshala frontend

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import Navbar from "../Navbar/Navbar"
+import { useCart } from 'react-use-cart'
 
 function Cart() {
+
+  const  {
+    isEmpty,
+    totalUniqueItems,
+    totalItems,
+    cartTotal,
+    items,
+    updateItemQuantity,
+    removeItem,
+    emptyCart,
+  } = useCart();
+
   return (
     <div>
-    <Navbar />
+      <Navbar />
+
+      <div>
+       {isEmpty ? 'Your cart is empty' : `Your products`}
+      </div>
+
     </div>
   )
 }

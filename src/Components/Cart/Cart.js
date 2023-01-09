2d39e45cd0cc28bd2da7be94bcae3a88d2c03b6b
Utkp5 <utkp200@gmail.com>
2023-01-09 20:49:18 +0530

    bookshala frontend

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -36,9 +36,30 @@ function Cart() {
       order_id: data.id,
       handler: async (response) => {
         // console.log(response,"5");
-        console.log(response.razorpay_payment_id);
-        console.log(response.razorpay_order_id);
-        console.log(response.razorpay_signature)
+        // console.log(response.razorpay_payment_id);
+        // console.log(response.razorpay_order_id);
+        // console.log(response.razorpay_signature)
+        try {
+          const verifyUrl = "https://moviebooking-utkarsh.herokuapp.com/api/Payment/verify";
+          const { data } = await axios.post(verifyUrl, response);
+          console.log(data);
+          Swal.fire({
+            text: 'Your Booking has been Confirmed',
+            imageUrl: 'https://cdn.dribbble.com/users/911154/screenshots/3332845/vfmov3.gif',
+            imageWidth: 300,
+            imageHeight: 200,
+            imageAlt: 'Custom image',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'OK'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.location.href = "/";
+              emptyCart();
+            }
+          })
+        } catch (error) {
+          console.log(error);
+        }
       },
     };
     const rzp1 = new window.Razorpay(options);
